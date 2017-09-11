4e06fc26c5c22a904085b28f8bb32ec4f23081ff
MopeyMantella <31834208+MopeyMantella@users.noreply.github.com>
2017-09-10 20:34:53 -0500

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -10,12 +10,31 @@ int main () {
   clock_t startTime = clock();
   cout << "Beginning loop" << endl;
 
-  for (int i = 3; i < MAX; i++) {
-    //this is where your algorithm code would go
-    i++; //do something to waste time...
-    i--; //do something to waste time...
+int main ()
+{     
+  clock_t startTime = clock();
+  cout << "Beginning loop" << endl;
+   
+  for (int i = 3; i < 51; i++) 
+  {
+    while (i % 2 == 0)
+    {
+      i = i/2;
+    }
+                
+    while (i % 2 == 1)
+    {
+      i = i * 3 + 1;
+      for (i % 2 == 0)
+        i = i/2;
+    }
+    for(i=1)
+      cout << "Magic, "
+    else
+      cout << "~Magic." << endl;  
   }
-
+}
+      
   clock_t finishTime = clock();
   cout << "Looping took " << double(finishTime - startTime) / CLOCKS_PER_SEC << " seconds" << endl;
 
